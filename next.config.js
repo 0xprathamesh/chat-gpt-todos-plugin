e0d0637200beb105985d5af657dc8eb17032566a
Prathamesh48 <prathameshpatil6499@gmail.com>
2023-04-27 18:44:44 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://chat.openai.com",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
